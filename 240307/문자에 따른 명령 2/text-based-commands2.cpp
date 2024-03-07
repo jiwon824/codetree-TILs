0cfa57750dd3848b2fca4c993c7866ed83b17dae
jiwon824 <59115860+jiwon824@users.noreply.github.com>
2024-03-07 19:42:35 +0900

    Title: 문자에 따른 명령 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240307/문자에 따른 명령 2/text-based-commands2.cpp
+++ 240307/문자에 따른 명령 2/text-based-commands2.cpp
@@ -14,7 +14,7 @@ int main() {
     string s;
     cin >> s;
     int x=0, y=0;
-    int dir = 0;
+    int dir = 3;
     for(int i =0; i<s.length();i++){
         if(s[i]=='L'){
             dir = (dir+3)%4;
