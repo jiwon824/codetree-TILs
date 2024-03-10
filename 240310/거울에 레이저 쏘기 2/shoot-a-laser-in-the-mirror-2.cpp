6ae46b8ef72383bb3d6d819aa96a579313a014d1
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-03-10 20:28:57 +0900

    Title: 거울에 레이저 쏘기 2, Time: 15ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240310/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 240310/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -9,7 +9,7 @@ char** map;
 // 시작위치 구하기
 int* FindStartPos(int k, int n){
     int* pos =new int[2];
-    int dir =1;
+    int dir =3;
     int x=0, y=0, time=0;
     while (k!=time){
         int nx =x+dx[dir];
@@ -28,25 +28,25 @@ int* FindStartPos(int k, int n){
 }
 
 int Reflect(int k, int n){
-    int cnt=1;
     // dir = n으로 나눠서 0:하, 1:좌, 2:상, 3:우
     int dir =k/n;
     int* startpos = FindStartPos(k, n);
     int nx =startpos[0], ny=startpos[1];
+    int cnt=0;
     // map을 벗어날 때까지 반복
     // [0]하 [1]좌 [2]상 [3]우
     while(nx>=0 && ny>=0 && nx<n && ny<n){
         if(map[nx][ny]=='/'){
-            // '/'의 경우 1. 하[0]->우[3], 우[3]->하[0] 2. 좌[1]->상[2], 상[2]->좌[1]
-            if(dir==0 || dir==2){
-                // 반시계 회전(하[0]->우[3], 상[2]->좌[1])
+            // '/'의 경우 상[2]->우[3], 좌[1]->하[0] 2. 우[3]->상[2], 하[0]->좌[1]
+            if(dir==1 || dir==3){
+                // 반시계 회전(좌[1]->하[0], 우[3]->상[2])
                 dir=(dir+3)%4;
             }else{
-                // 시계방향 회전(우[3]->하[0], 좌[1]->상[2])
+                // 시계방향 회전(하[0]->좌[1], 상[2]->우[3])
                 dir=(dir+1)%4;
             }
         }else{
-            // '\'의 경우 1. 좌->하, 하->좌 2. 우->상, 상->우
+            // '\'의 경우 우[3]->하[0], 하[0]->우[3] 상[2]->좌[1] 좌[1]->상[2]
             if(dir==0 || dir==2){
                 // 시계방향 회전(하[0]->좌[1], 상[2]->우[3])
                 dir=(dir+1)%4;
@@ -55,9 +55,9 @@ int Reflect(int k, int n){
                 dir=(dir+3)%4;
             }
         }
+        cnt++;
         nx+=dx[dir];
         ny+=dy[dir];
-        cnt++;
     }
     
     return cnt;
