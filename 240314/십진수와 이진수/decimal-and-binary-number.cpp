be1557b973aae25150b3cfa0038230088ce4f30d
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-03-14 19:55:40 +0900

    Title: 십진수와 이진수, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240314/십진수와 이진수/decimal-and-binary-number.cpp
+++ 240314/십진수와 이진수/decimal-and-binary-number.cpp
@@ -5,26 +5,25 @@ int main() {
     // 자연수이며 최대 자리수가 1000 자리인, 이진수로 표현된 숫자 N이 공백 없이 주어집니다.
     string n;
     cin >>n;
-    string answer =n;
-    answer= answer + "0000";
-    n="0000"+n;
+    string a=n+"0000", b="0000"+n, answer="";
     int carry =0;
-    for(int i=answer.length()-1;i>=0;i--){
-        int tmp =(answer[i]-'0')+(n[i]-'0')+carry;
+    for(int i=a.length()-1;i>=0;i--){
+        int tmp =(a[i]-'0')+(b[i]-'0')+carry;
         switch(tmp){
             case 0:
+                answer = '0'+answer;
                 carry=0;
                 break;
             case 1:
-                answer[i] = '1';
+                answer = '1'+answer;
                 carry = 0;
                 break;
             case 2:
-                answer[i]='0';
+                answer='0'+answer;
                 carry=1;
                 break;
             case 3:
-                answer[i]='1';
+                answer='1'+answer;
                 carry =1;
                 break;
             default:
