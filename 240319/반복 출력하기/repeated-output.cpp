93b119ffba5842e97361a3d0e3feb21096d2f723
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-03-19 16:35:02 +0900

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240319/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+void Print(int num){
+    for (int i=0;i<num;i++){
+        cout << "12345^&*()_\n";
+    }
+}
+int main() {
+    int n;
+    cin >>n;
+    Print(n);
+    return 0;
+}
