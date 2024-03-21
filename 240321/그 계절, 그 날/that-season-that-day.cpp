03c32b16a807df78897b694b19f40aa341823a16
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-03-21 13:44:49 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240321/그 계절, 그 날/that-season-that-day.cpp
+++ 240321/그 계절, 그 날/that-season-that-day.cpp
@@ -16,10 +16,11 @@ bool IsLeapYear(int year){
 int main() {
     int y, m, d;
     cin >> y >> m >> d;
-    // 1, 3, 5, 7, 8, 10, 12월은 31일까지
+    // 2월은 윤년
     // 4, 6, 9, 11월은 30일까지
+    // 1, 3, 5, 7, 8, 10, 12월은 31일까지
     if (m==2){
-        if(d==29){
+        if(d>=29){
             if(IsLeapYear(y)) PrintSeason(m);
             else cout <<"-1";
         }
