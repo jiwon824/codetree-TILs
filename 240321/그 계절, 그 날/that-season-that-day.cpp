b4e987f4c3c6232dc798c7a05fab55be12bf4fa7
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-03-21 13:35:29 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240321/그 계절, 그 날/that-season-that-day.cpp
+++ 240321/그 계절, 그 날/that-season-that-day.cpp
@@ -19,13 +19,13 @@ int main() {
     // 홀수 달은 31일이 존재 짝수 달은 31일이 없음
     if(m%2==0){
         if(m==2 && d==29){
-            if(IsLeapYear(y)) PrintSeason(y);
+            if(IsLeapYear(y)) PrintSeason(m);
             else cout <<"-1";
         }
         else if(d==31) cout << "-1";
-        else PrintSeason(y);
+        else PrintSeason(m);
     }
-    else PrintSeason(y);
+    else PrintSeason(m);
 
     return 0;
 }
