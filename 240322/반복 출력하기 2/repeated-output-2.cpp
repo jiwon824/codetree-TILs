17b7e3325b95145cbe11bc26a9c4f2ebcaa4f092
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-03-22 16:53:10 +0900

    Title: 반복 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240322/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void Print(int n){
+    if(n==0) return;
+    Print(n-1);
+    cout << "HelloWorld\n";
+}
+int main() {
+    int n;
+    cin >> n;
+    Print(n);
+    return 0;
+}
