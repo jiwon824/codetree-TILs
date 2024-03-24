d8244d948c0794abbb780256a71dcebe8a48afad
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-03-24 15:38:02 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240324/Factorial/factorial.cpp
+++ 240324/Factorial/factorial.cpp
@@ -2,9 +2,7 @@
 
 using namespace std;
 int Factorial(int n){
-    if(n<=1) return 1;
-    if(n==2) return 2;
-    if(n==3) return 6;
+    if(n==0) return 1;
     return n * Factorial(n-1);
 }
 int main() {
