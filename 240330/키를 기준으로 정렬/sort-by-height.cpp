1d7fd0d5abd5cbce0cc590436e8d502ba13b3d9d
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-03-30 19:54:27 +0900

    Title: 키를 기준으로 정렬, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240330/키를 기준으로 정렬/sort-by-height.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Student{
+    public:
+        string name;
+        int h ,w;
+    Student(){};
+    Student(string name, int h, int w){
+        this->name = name;
+        this->h = h;
+        this->w = w;
+    }
+}; 
+
+bool cmp(Student a, Student b){
+    return a.h < b.h;
+}
+int main() {
+    int n;
+    cin >> n;
+    Student students[n];
+    for(int i=0;i<n;i++){
+        string name;
+        int h, w;
+        cin >> name >> h >> w;
+        students[i] = Student(name, h, w);
+    }
+    sort(students, students+n, cmp);
+    
+    for(int i = 0; i < n; i++)
+        cout << students[i].name << " " << students[i].h << " " << students[i].w << '\n';
+    return 0;
+}
