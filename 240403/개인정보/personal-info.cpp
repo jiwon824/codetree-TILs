2cc372ad834743cdf1e99df25422b1c8661914ec
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-03 18:27:58 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240403/개인정보/personal-info.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Data{
+    public:
+        string name;
+        int height;
+        float weight;
+    Data(){};
+    Data(string name, int height, float weight){
+        this->name = name;
+        this->height = height;
+        this->weight = weight;
+    }
+};
+
+bool CmpWithHeight(Data a, Data b){
+    return a.height > b.height;
+}
+bool CmpWithName(Data a, Data b){
+    return a.name < b.name;
+}
+int main() {
+    Data d[5];
+    for(int i=0;i<5;i++){
+        string n;
+        int h;
+        float w;
+        cin >> n >> h >> w;
+        d[i] = Data(n, h, w);
+    }
+    
+    cout <<"name\n";
+    sort(d, d+5, CmpWithName);
+    for(int i=0;i<5;i++){
+        cout << d[i].name << " " <<d[i].height << " " <<d[i].weight<<'\n'; 
+    }
+    cout <<"\nheight\n";
+    sort(d, d+5, CmpWithHeight);
+    for(int i=0;i<5;i++){
+        cout << d[i].name << " " <<d[i].height << " " <<d[i].weight<<'\n'; 
+    }
+    return 0;
+}
