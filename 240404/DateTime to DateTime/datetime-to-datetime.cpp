234cbe09195c741c594596ae12cd2b77005221db
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-04 19:34:29 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240404/DateTime to DateTime/datetime-to-datetime.cpp
+++ 240404/DateTime to DateTime/datetime-to-datetime.cpp
@@ -7,7 +7,22 @@ int main() {
     //11일 11시 11분에서 시작하여 a일 b시 c분까지 몇 분이 걸리는지
     int day=11, time=11, min=11, answer=0;
     while (true){
-        if(day==a && time ==b && min ==c) break;
+        // 단, a일 b시 c분이 11일 11시 11분보다 더 앞서다면(=작다면) -1을 출력
+        if(a<11){
+            cout << "-1";
+            break;
+        }
+        if (a==11 && b<11){
+            cout << "-1";
+            break;
+        }
+        if (a==11 && b==11 && c<11){
+            cout << "-1";
+            break;
+        }
+
+        
+        if(day==a && time==b && min==c) break;
         min++;
         answer++;
 
@@ -20,6 +35,9 @@ int main() {
             time=0;
         }
     }
-    cout << answer;
+    
+    if (a==11 && b==11 && c==11) cout << "0";
+    else if(answer!=0) cout << answer;
+    
     return 0;
 }
