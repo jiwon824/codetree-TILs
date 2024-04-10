d17e120cb94822aa83f7967a4b8bdeb1d17b3468
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-10 20:38:01 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240410/괄호 쌍 만들어주기 3/pair-parentheses-3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    string a;
+    cin >> a;
+    int answer =0;
+    for (int i=0; i<a.length();i++){
+        if (a[i]=='('){
+            for (int j=i+1;j<a.length();j++){
+                if(a[j]==')'){
+                    answer++;
+                }
+            }
+        }
+    }
+    cout << answer;
+    return 0;
+}
