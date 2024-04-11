8b02f6b1208a7a89b0aace279b579033c4bc4a3c
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-11 22:38:37 +0900

    Title: 색종이의 총 넓이, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240411/색종이의 총 넓이/the-total-area-of-colored-paper..cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+int arr[201][201];
+void Draw(int a, int b){
+    for(int i=a;i<a+8;i++){
+        for(int j=b;j<b+8;j++){
+            arr[i][j]=1;
+        }
+    }
+}
+int main() {
+    int n; // 가로세로 길이가 8이고 넓이가 64인 색종이 N장
+    cin >> n;
+    for(int i =0; i<n; i++){
+        int x1, y1;
+        cin >> x1 >> y1;
+        Draw(x1, y1);
+    }
+
+    int answer =0;
+    for(int i=0; i<201; i++){
+        for(int j=0; j<201; j++){
+            if(arr[i][j]==1) answer++;
+        }
+    }
+    cout << answer;
+    return 0;
+}
