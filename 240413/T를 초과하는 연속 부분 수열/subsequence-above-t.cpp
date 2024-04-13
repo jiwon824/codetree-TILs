a9df17a491470987f48b830c3972acdd4763b6d5
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-13 21:02:37 +0900

    Title: T를 초과하는 연속 부분 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240413/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
+++ 240413/T를 초과하는 연속 부분 수열/subsequence-above-t.cpp
@@ -5,24 +5,28 @@ int main() {
     int n, t;
     cin >> n >> t;
     int *arr = new int[n];
+    bool exist = false;
     for(int i=0; i<n; i++){
         cin >> arr[i];
+        if(arr[i]>t) exist =true;
     }
 
     int max_len =1, len=1;
-    bool exist = false;
-    for(int i=1; i<n; i++){
-        if(arr[i-1]>t && arr[i]>t){
-            len++;
-            exist = true;
+    if(exist){
+        for(int i=1; i<n; i++){
+            if(arr[i-1]>t && arr[i]>t){
+                len++;
+                exist = true;
+            }
+            else{
+                max_len = max(max_len, len);
+                len=1;
+            }
         }
-        else{
-            max_len = max(max_len, len);
-            len=1;
-        }
+        max_len = max(max_len, len);
     }
-    max_len = max(max_len, len);
-    if(exist) cout << max_len;
-    else cout << '0';
+    else max_len = 0;
+    
+    cout << max_len;
     return 0;
 }
