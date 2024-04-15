c82f88ec8f6b574ed1cc6ce14a705afa613cba19
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-16 00:09:49 +0900

    Title: 체크판위에서 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240416/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240416/체크판위에서 2/on-the-checkboard-2.cpp
@@ -12,8 +12,8 @@ int main() {
     }
 
     int answer=0;
-    for(int i=0;i<r;i++){
-        for(int j=0; j<c; j++){
+    for(int i=1;i<r;i++){
+        for(int j=1; j<c; j++){
             for(int k=i+1; k<r-1; k++){// 아래로 한 칸
                 for(int l=j+1; l<c-1; l++){ // 오른쪽으로 한 칸
                     if(arr[0][0]!=arr[i][j] && arr[i][j]!=arr[k][l] && arr[k][l]!=arr[r-1][c-1]) answer++;
