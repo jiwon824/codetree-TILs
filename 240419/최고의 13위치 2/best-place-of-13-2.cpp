71233f1198ea23b54329060ad958735152055d7e
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-19 15:02:44 +0900

    Title: 최고의 13위치 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240419/최고의 13위치 2/best-place-of-13-2.cpp
+++ 240419/최고의 13위치 2/best-place-of-13-2.cpp
@@ -17,9 +17,11 @@ int main() {
         for(int j=0;j<n-2;j++){
             for(int k=0; k<n; k++){
                 for(int l=0; l<n-2; l++){
-                    if(i==k && (j==l || j+1==l || j+2==l)) continue;
+                    if(i==k &&((j==l || j+1==l || j+2==l) || (l==j || l+1==j || l+2==j))) continue;
                     int sum_ij = arr[i][j]+arr[i][j+1]+arr[i][j+2];
                     int sum_kl = arr[k][l]+arr[k][l+1]+arr[k][l+2];
+                    //cout << "ij "<< i << "/ "<< j << " " << j+1 << " "<< j+2 << '\n';
+                    //cout << "kl "<< k << "/ "<< l << " " << l+1 << " "<< l+2 << '\n';
                     answer=max(answer, sum_ij+sum_kl);
                 }
             }
