088db7deb330c6f95b75f19f1a38a7ed1dda7530
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-21 19:11:41 +0900

    Title: 밭의 높이를 고르게하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240421/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 240421/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -14,24 +14,23 @@ int main() {
 
     // 연속 t개 이상의 밭이 h 높이가 되도록 만들기
     int min_cost = INT_MAX;
-    for(int i=0; i<n; i++){
-        int cost=0, num_of_h =0;
-        for(int j=i;j<n;j++){
-            if(arr[j]==h){
-                num_of_h++;
-            }
+    for(int i=0; i<=n-t; i++){
+
+        int cost=0;
+        for(int j=i; j<i+t; j++){
+            //cout << i <<" " << j <<'\n';
+            if(arr[j]==h) continue;
             else if(arr[j]<h){
                 cost += h-arr[j];
-                num_of_h++;
             }
             else{
                 cost += arr[j]-h;
-                num_of_h++;
             }
-            if(num_of_h>=t) break;
         }
+
         if(cost<min_cost) min_cost = cost;
     }
+
     cout << min_cost;
     return 0;
 }
