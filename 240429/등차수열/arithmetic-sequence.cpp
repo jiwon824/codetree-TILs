b3e1564886731b34b48187a149da4b20019cb852
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-29 12:09:49 +0900

    Title: 등차수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240429/등차수열/arithmetic-sequence.cpp
+++ 240429/등차수열/arithmetic-sequence.cpp
@@ -11,18 +11,20 @@ int main() {
         cin >> arr[i];
     }
 
-    // (ai, k, aj)가 등차수열을 이룬다는 말은 aj-k와 k-ai가 동일하다는 것
+    // (aj, i, ak)가 등차수열을 이룬다는 말은 ak-i와 i-aj가 동일하다는 것
     int answer=0;
-    for (int i =-100; i<200; i++){
+    for (int i =0; i<100; i++){
+        int num_of_ap =0;
         // 1<=j<k<=n 
         for(int j =0; j<n; j++){
             for(int k=j+1; k<n; k++){
-                if(arr[j]-i == i-arr[k]) {
+                if(arr[k]-i == i-arr[j]) {
                     //cout << arr[j] << " " << i << " "<< arr[k]<<'\n';
-                    answer++;
+                    num_of_ap++;
                 }
             }
         }
+        if(answer<num_of_ap)answer=num_of_ap;
     }
     cout << answer;
     return 0;
