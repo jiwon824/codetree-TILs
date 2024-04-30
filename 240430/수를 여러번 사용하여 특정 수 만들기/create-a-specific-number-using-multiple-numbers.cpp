f9cfe8ef23fe006302e0ad5a5ab819273a48730e
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-04-30 12:48:19 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240430/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
+++ 240430/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -7,7 +7,7 @@ int main() {
 
     // A와 B를 0번 이상 더해서 만들 수 있는 C 이하인 수 중에서 가장 큰 값
     int answer =0;
-    for(int i=0; i<c/a;i++){
+    for(int i=0; i<c;i++){
         for(int j=0; j<1000; j++){
             int num = a*i+b*j;
             if(num>c) break;
