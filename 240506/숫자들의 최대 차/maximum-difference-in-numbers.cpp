26817979a9de7b3e7b68d35b8f1c36c511672bf5
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-05-06 13:46:26 +0900

    Title: 숫자들의 최대 차, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240506/숫자들의 최대 차/maximum-difference-in-numbers.cpp
+++ 240506/숫자들의 최대 차/maximum-difference-in-numbers.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
+
 int n, k;
 int arr[1001];
-
 
 int main() {
     cin >> n >> k;
     for(int i=0; i<n; i++){
         cin >> arr[i];        
     }
-    
+
+    sort(arr, arr+n);
+
     int answer =0;
-    // i가 최솟값인 경우 뽑을 수 있는 숫자의 갯수
+    // arr[i]가 최솟값인 경우 뽑을 수 있는 숫자의 갯수
     for(int i=0;i<n;i++){
         int cnt=1;
         for(int j=i+1; j<n; j++){
-            if(arr[j]-arr[i]>=0 && arr[j]-arr[i]<=k) {
-                //cout << arr[i] << " "<< arr[j] << " " << arr[j]-arr[i]<<'\n';
-                cnt++;
-            }
-
+            if(arr[i]+k >= arr[j]) cnt++;
         }
         if(answer<cnt) answer =cnt;
     }
