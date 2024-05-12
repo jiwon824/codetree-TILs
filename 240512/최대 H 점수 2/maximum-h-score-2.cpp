3b8a3f0880156eeee8d9b112cec43ea13af5dd8a
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-05-12 20:24:44 +0900

    Title: 최대 H 점수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240512/최대 H 점수 2/maximum-h-score-2.cpp
+++ 240512/최대 H 점수 2/maximum-h-score-2.cpp
@@ -1,59 +1,10 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int arr[101];
-int visited[101];
-vector<int> comb;
 int n, l;
-int answer;
-
-int Check_Score(){
-    // arr[comb[i]]++해주기
-    for(int i=0; i<comb.size(); i++){
-        arr[comb[i]]++;
-    }
-
-    int num_of_score=0;
-    for(int i=0; i<101; i++){
-        int num_of_i=0;
-        for(int j=0; j<n; j++){
-            if(arr[j]>=i) num_of_i++;
-        }
-        //cout << num_of_i << '\n';
-        // i이상인 수의 갯수가 i개 이상이면서 num_of_score<i라면 num_of_score 갱신
-        if(num_of_i>=i && num_of_score<i) num_of_score=i; 
-    }
-
-    // arr[comb[i]]-- 원상복구
-    for(int i=0; i<comb.size(); i++){
-        arr[comb[i]]--;
-    }
-    return num_of_score;
-}
-void Make_Comb(int curr_num){
-    if(curr_num==l){
-        //for(auto e : comb){
-        //    cout << e << " ";
-        //}
-        //cout << '\n';
-
-        int score = Check_Score();
-        if(answer<score) answer=score;
-        return;
-    }
-
-    for(int i=0; i<n; i++){
-        if(visited[i]) continue;
-
-        comb.push_back(i);
-        visited[i]=1;
-        Make_Comb(curr_num+1);
-        comb.pop_back();
-        visited[i]=0;
-    }
-}
 
 int main() {
     cin >> n >> l;
@@ -61,7 +12,29 @@ int main() {
         cin >> arr[i];
     }
 
-    Make_Comb(0);
-    cout << answer;
+    // 내림차순으로 정렬
+    sort(arr, arr+n, greater<int>());
+    //for(int i=0; i<n ;i++){
+    //    cout << arr[i] << " ";
+    //}
+    //cout <<'\n';
+    
+    // i이상인 수가 i개 이상 나오면 cout<< i; break; 하면 됨
+    for(int i=n; i>=0; i--){
+        int num_of_over_i=0, l_cnt=l;
+        for(int j=0; j<n; j++){
+            if(arr[j]>=i) num_of_over_i++;
+            else if(arr[j]==i-1 && l_cnt>0){
+                l_cnt--;
+                num_of_over_i++;
+            }
+        }
+
+        if (num_of_over_i>=i) {
+            cout << i << '\n';
+            break;
+        }
+    }
+
     return 0;
 }
