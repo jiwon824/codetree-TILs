877cc2a705fb646405ab9c06e3796e3530360ea3
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-05-13 20:49:49 +0900

    Title: 순위 경쟁2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240513/순위 경쟁2/ranking-competition2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a=0, b=0;
+    int answer=0;
+    for(int i=0;i<n; i++){
+        char person;
+        int score;
+        cin >>person >> score;
+        int na=a, nb=b;
+        if(person =='A'){
+            na+=score;
+        }
+        else{
+            nb+=score;
+        }
+        // 변동이 없을 경우 아무것도 하지 않음
+        if((a>b && na>nb) || (b>a && nb>na)) {
+            // do nothing
+        }
+        else{
+            answer++;
+        }
+        a=na;
+        b=nb;
+    }
+    cout << answer;
+    return 0;
+}
