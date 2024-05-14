1c82f572fdce84f176361e1ec9734903b125b90e
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-05-14 12:15:48 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240514/코딩톡/coding-talk.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<pair<char, int>> msg;
+bool isRead[27];
+
+int main() {
+    int n, m, p;
+    cin >> n >> m >> p;
+    for(int i=0; i<m; i++){
+        char c;
+        int u;
+        cin >> c >> u;
+        msg.push_back({c, u});
+    }
+
+    for(int i=p-1; i<m; i++){
+        int sender = msg[i].first -'A';
+        isRead[sender] = true;
+    }
+    
+    for(int i=0; i<n; i++){
+        char reader = (char)i+'A';
+        if(isRead[i]==false) cout << reader << " ";
+    }
+
+    return 0;
+}
