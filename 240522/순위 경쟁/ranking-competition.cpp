b0b86d09f86250b51c8766a39ae7704e7f655dcd
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-05-22 19:39:24 +0900

    Title: 순위 경쟁, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240522/순위 경쟁/ranking-competition.cpp
+++ 240522/순위 경쟁/ranking-competition.cpp
@@ -4,47 +4,31 @@ using namespace std;
 
 int score[3];
 
-bool Is_Change_Hall_of_Fame(string past, string curr){
-
-    int past_max_score = max(past[0], max(past[1], past[2]));
-    int past_hall_of_fame[3]={0, };
+string Winner_list(){
+    int max_score = max(score[0], max(score[1], score[2]));
+    string winner="000";
     for(int i=0; i<3; i++){
-        if(past[i]==past_max_score) past_hall_of_fame[i]=1;
+        if(score[i]==max_score) winner[i]=1;
     }
-
-    int curr_max_score = max(curr[0], max(curr[1], curr[2]));
-    int curr_hall_of_fame[3]={0, };
-    for(int i=0; i<3; i++){
-        if(curr[i]==curr_max_score) curr_hall_of_fame[i]=1;
-    }
-
-    for(int i=0; i<3; i++){
-        // 바뀌었으면 Change= true;
-        if(past_hall_of_fame[i]!=curr_hall_of_fame[i]) return true;
-    }
-    // return된 적 없으면 안 바뀜 false;
-    return false;
+    return winner;
 }
 
 int main() {
     int n;
     cin >> n;
 
-    string recent_scores="000";
     int cnt =0;
     for(int i=0; i<n; i++){
         char person;
         int input_score;
         cin >> person >>input_score;
+        // 이전의 승자 목록
+        string recent_winner_list = Winner_list();
+        score[person-'A']+=input_score; // input_score 더하기
+        // input_score를 더한 후 승자 목록
+        string curr_winner_list = Winner_list();
 
-        score[person-'A']+=input_score;
-        string curr_score ="";
-        for(int j=0; j<3; j++){
-            char ch_j = score[j]+'0'; // -1이 '/'로 표현되는 문제가 있으나 문제 풀이에는 지장이 없다
-            curr_score= curr_score+ch_j;
-        }
-        if(Is_Change_Hall_of_Fame(recent_scores, curr_score)) cnt++;
-        recent_scores = curr_score;
+        if(recent_winner_list!=curr_winner_list) cnt++;
     }
 
     cout << cnt<< '\n';
