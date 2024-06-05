750b74196763c28d07dfdc1a6a057e15fe550ab9
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-06-05 11:32:09 +0900

    Title: 기울어진 직사각형의 회전, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240605/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
+++ 240605/기울어진 직사각형의 회전/rotate-slanted-rectangle.cpp
@@ -41,24 +41,24 @@ void Rotate(){
         //shift line2
         // [r-m1][c+m1]에서 좌상(-1,-1)으로 height 만큼 가야함
         int lastElementOfL2=arr[r-m1-m2][c+m1-m2];
-        for(int i=m2; i>1; i--){
+        for(int i=m2; i>0; i--){
             //우하(1,1)에 있는 원소를 현재 칸에 넣어야 함
-            arr[r-m1-i+1][c+m1-i+1]=arr[r-m1-i][c+m1-i];
+            arr[r-m1-i][c+m1-i]=arr[r-m1-i+1][c+m1-i+1];
         }
         arr[r-m1-1][c+m1-1]=lastElementOfL1;
 
         //shift line3
         // r,c에서 좌상으로 hight만큼 간 값
         int lastElementOfL3=arr[r-m4][c-m4];
-        for(int i=0; i<m3-1; i++){
+        for(int i=0; i<m3; i++){
             // 우상(-1,1)에 있는 값을 마지막 값에서부터 당기기
             arr[r-m4-i][c-m4+i]=arr[r-m4-i-1][c-m4+i+1];
         }
         arr[r-m1-m2+1][c+m1-m2-1]=lastElementOfL2;
 
         //shift line4
-        for(int i=1; i<m4; i++){
-            arr[r][c]=arr[r-i][c-i];
+        for(int i=0; i<m4; i++){
+            arr[r-i][c-i]=arr[r-i-1][c-i-1];
         }
         arr[r-m4+1][c-m4+1]=lastElementOfL3;
 
