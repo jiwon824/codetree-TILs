d2bf7c6865b4404784f81b7143a11381ad4a54b3
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-06-07 06:58:53 +0900

    Title: 십자 모양 폭발, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240607/십자 모양 폭발/cross-shape-bomb.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+int n;
+int arr[201][201]; //1이상 100이하의 숫자로 구성
+
+void PrintArr(){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << '\n';
+    }
+}
+
+void GravitationalActions(){
+    for(int col=0; col<n; col++){
+        for(int row=n-1; row>0; row--){
+            if(arr[row][col]==0){
+                arr[row][col]=arr[row-1][col];
+                arr[row-1][col]=0;
+            }
+        }
+    }
+}
+
+void Explode(int r, int c){
+    // 좌(0,-1)상(-1,0)우(0,1)하(1,0)
+    int dx[4]={0, -1, 0, 1};
+    int dy[4]={-1, 0, 1, 0};
+    
+    int explosionRange = arr[r][c]-1;
+    for(int dir=0; dir<4; dir++){
+        if(explosionRange==0) break;
+
+        int x=r, y=c;
+        for(int move=0; move<explosionRange; move++){
+            x+=dx[dir];
+            y+=dy[dir];
+            if(x<0 || x>=n || y<0 || y>=n) break; //같은 방향으로 가니까 한 번 범위를 벗어나면 쭉 벗어남
+            arr[x][y]=0; // 범위를 벗어나지 않으면 폭발시키기
+        }
+
+    }
+    arr[r][c]=0;
+    
+    GravitationalActions();
+}
+
+int main() {
+    //input
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> arr[i][j];
+        }
+    }
+    int r, c;
+    cin >> r >> c;
+
+    Explode(r-1, c-1);
+    
+    PrintArr();
+    return 0;
+}
