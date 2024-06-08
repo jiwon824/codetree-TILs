bb1f0db377188a7c676463a7ccec70100aa1d72b
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-06-08 13:28:35 +0900

    Title: 1차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240608/1차원 폭발 게임/The-1D-bomb-game.cpp
+++ 240608/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -6,44 +6,43 @@ int n, m;
 int bombs[101]; // 1이상 100이하의 숫자가 적혀있는 N개의 폭탄
 
 // 배열의 원소들을 앞으로 당기는 
-void MakeCompact(int startIdx){
-    // 원소들 앞으로 당기기
-    for(int i=startIdx; i<n; i++){
-        if(bombs[i]!=0){
-            bombs[startIdx]=bombs[i];
-            bombs[i]=0;
-            startIdx++;
+void MakeCompact(){
+    // 빈 공간 찾기
+    int endOfIdx=0;
+    for(int i=0; i<n; i++){
+        if(bombs[i]==0) {
+            endOfIdx=i;
+            break;
         }
     }
 
-    // 배열 갯수 세기
-    int cnt=0;
-    for(int i=0; i<n; i++){
-        if(bombs[i]==0) break;
-        cnt++;
+    // 원소들 앞으로 당기기
+    for(int i=endOfIdx; i<n; i++){
+        if(bombs[i]!=0){
+            bombs[endOfIdx]=bombs[i];
+            bombs[i]=0;
+            endOfIdx++;
+        }
     }
-    n=cnt;
+    
+    // 배열 갯수 세기
+    n=endOfIdx;
 }
 
-int IdxToStartExploding(){
+bool IsExploding(){
     bool isExplode=false;
 
-    int cnt=1, idx=0;
+    int cnt=1;
     // 폭발될 숫자가 있나 확인
     for(int i=1; i<n; i++){
-        if(bombs[i-1]==bombs[i])cnt++;
-        else {
-            idx=i;
-            cnt=1;
-        }
+        if(bombs[i-1]==bombs[i]) cnt++;
+        else cnt=1;
         if(cnt>=m){
             isExplode = true;
             break;
         }
     }
-
-    if(isExplode) return idx;
-    else return -1;
+    return isExplode;
 }
 
 // m개 이상 연속 같은 숫자가 있으면 해당 숫자 폭탄이 전부 터짐
@@ -53,17 +52,28 @@ void Explode(){
             n=0;
             break;
         }
-        // 지울 숫자가 처음 나오는 idx
-        int start=IdxToStartExploding();
-        if(start==-1) break; // -1이면 지울 수가 없다는 의미
-        // 지울 숫자
-        int removeNum=bombs[start];
-        for(int i=start; i<n; i++){
-            if(bombs[i]!=removeNum) break;
-            bombs[i]=0;
+        if(!IsExploding()) break;
+
+        //M개 이상인 폭탄들의 쌍이 여러 개라면 동시에 터지게 됩니다.
+        int cnt=1, sIdx=0;
+        for(int i=1; i<n; i++){
+            if (bombs[i]==0) continue;
+            if (bombs[i-1]==bombs[i]) cnt++;
+            else {
+                cnt=1;
+                sIdx=i;
+            }
+
+            if(cnt>=m){
+                int removeNum=bombs[sIdx];
+                for(int j=sIdx; j<n; j++){
+                    if(bombs[j]!=removeNum) break;
+                    bombs[j]=0;
+                }
+            }
         }
 
-        MakeCompact(start);
+        MakeCompact();
     }
 }
 
