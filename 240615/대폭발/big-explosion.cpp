20378495ee83143a7c2c133e20a09d31eea452da
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-06-15 19:00:45 +0900

    Title: 대폭발, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240615/대폭발/big-explosion.cpp
+++ 240615/대폭발/big-explosion.cpp
@@ -25,7 +25,8 @@ void PlaceBombs(){
             for(int dir=0; dir<4; ++dir){
                 int nx= x+(dx[dir]*pow(2, time-1));
                 int ny= y+(dy[dir]*pow(2, time-1));
-                if(nx<0 || nx>=n || ny<0 || ny>=n) continue;
+                // 범위를 벗어나거나 이미 방문했던 곳이라면 continue
+                if(nx<0 || nx>=n || ny<0 || ny>=n || arr[nx][ny]==1) continue;
                 bombs.push({nx, ny});
                 arr[nx][ny]=1;
             }
