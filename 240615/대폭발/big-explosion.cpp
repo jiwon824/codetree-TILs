e72034bbecc84b5f5be0ad8b6309066867166664
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-06-15 18:53:00 +0900

    Title: 대폭발, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240615/대폭발/big-explosion.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <queue>
+#include <cmath>
+using namespace std;
+
+int n, m, r, c;
+int arr[101][101];
+
+void PlaceBombs(){
+    //좌(0,-1) 상(-1,0) 우(0,1) 하(1,0)
+    int dx[4]={0, -1, 0, 1};
+    int dy[4]={-1, 0, 1, 0};
+
+    queue <pair<int, int> > bombs;
+    bombs.push({r, c}); // 0초에는 (r,c)에만 폭탄이 있음
+    arr[r][c]=1;
+    
+    for(int time=1; time<=m; ++time){
+        int size=bombs.size();
+        for(int qSize=0; qSize<size; ++qSize){
+            int x = bombs.front().first;
+            int y = bombs.front().second;
+            bombs.pop();
+
+            for(int dir=0; dir<4; ++dir){
+                int nx= x+(dx[dir]*pow(2, time-1));
+                int ny= y+(dy[dir]*pow(2, time-1));
+                if(nx<0 || nx>=n || ny<0 || ny>=n) continue;
+                bombs.push({nx, ny});
+                arr[nx][ny]=1;
+            }
+            bombs.push({x, y});
+        }
+    }
+    
+}
+
+int main() {
+    cin >> n >> m >> r >>c ;
+    r-=1;
+    c-=1;
+    PlaceBombs();
+
+    int answer=0;
+    for(int i=0; i<n; ++i){
+        for(int j=0; j<n; ++j){
+            if(arr[i][j]==1) answer++;
+        }
+    }
+    cout << answer;
+    return 0;
+}
