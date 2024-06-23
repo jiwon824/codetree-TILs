2f14d4f82fd014f9867528ab8969c6bc70456fb7
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-06-23 17:30:40 +0900

    Title: 최소 점프 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240623/최소 점프 횟수/min-num-of-jumps.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+int n, answer=INT_MAX;
+int numOfJump[11];
+vector<int> jump;
+
+void Simulate(int idx, int movement){
+    // 종료 조건
+    if(idx==n-1 || movement==0){
+        if(idx==n-1) answer = min(answer, static_cast<int>(jump.size()));
+        if(idx<n-1 && movement==0) answer = -1;
+        return;
+    }
+
+    // 재귀 호출
+    for(int i=1; i<=movement; i++){
+        jump.push_back(idx+i);
+        Simulate(idx+i, numOfJump[idx+i]);
+        jump.pop_back();
+    }
+}
+
+int main() {
+    // input
+    cin >> n;
+    for(int i=0; i<n; ++i){
+        cin >> numOfJump[i];
+    }
+
+    Simulate(0, numOfJump[0]);
+
+    // output
+    cout << answer << '\n';
+    return 0;
+}
