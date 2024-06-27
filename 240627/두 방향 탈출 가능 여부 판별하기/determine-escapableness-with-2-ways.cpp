80d8b0ec06619ebe01d354e94813d9c4a0ef5250
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-06-27 15:42:54 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240627/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
+++ 240627/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -4,31 +4,29 @@ using namespace std;
 
 int n, m, answer;
 int arr[101][101];// 각 행에 뱀이 없는 경우 1, 뱀이 있는 경우 0
-bool visited[101][101];
+bool visited[101][101] = {false, };
 
 bool InRange(int x, int y){
-    return x>=0 && x<n && y>=0 && y<n;
+    return x>=0 && x<n && y>=0 && y<m;
 }
 
 bool CanGo(int x, int y){
     // 범위를 벗어남
     if(!InRange(x, y)) return false;
     // 뱀이 있음 || 이미 방문한 칸임
-    if(arr[x][y]==0 || visited[x][y]) return false;
+    if(arr[x][y]==0 || visited[x][y]==true) return false;
     return true;
 }
 
 void DFS(int x, int y){
-    // 우(0,1) 하(1,0)
-    int dx[2]={0, 1};
-    int dy[2]={1, 0};
+    // 하(1,0) 우(0,1)
+    int dx[2]={1, 0};
+    int dy[2]={0, 1};
 
     for(int dir=0; dir<2; ++dir){
-        int nx = x+dx[dir];
-        int ny = y+dy[dir];
+        int nx = x+dx[dir], ny = y+dy[dir];
         
         if(nx==n-1 && ny==m-1) answer=1;
-        
         if(CanGo(nx, ny)){
             visited[nx][ny]=true;
             DFS(nx, ny);
