3b98e83d3cc5cae793f722bad162c1a317f4a472
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-07-01 13:44:37 +0900

    Title: 서로 다른 BST 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240701/서로 다른 BST 개수 세기/number-of-unique-bst.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[20];
+
+int main() {
+    int n;
+    cin >> n;
+    dp[0]=1;
+    dp[1]=1;
+
+    for(int i=2; i<=n; ++i){
+        for(int j=0; j<i; ++j){
+            dp[i]+=dp[j]*dp[i-j-1];
+        }
+    }
+
+    cout << dp[n] << '\n';
+
+    return 0;
+}
