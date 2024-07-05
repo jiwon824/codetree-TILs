2393a4f1b67ddf05af48db73b00ec817256cf61a
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-07-05 13:48:33 +0900

    Title: 최대 증가 부분 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240705/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240705/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -11,10 +11,10 @@ int main() {
     cin >> n;
     for(int i=0; i<n; ++i){
         cin >> arr[i];
+        dp[i]=1; //dp 1로 초기화
     }
 
     // solution
-    dp[0]=1;
     for(int i=1; i<n; ++i){
         for(int j=0; j<i; ++j){
             if(arr[j]<arr[i]){
