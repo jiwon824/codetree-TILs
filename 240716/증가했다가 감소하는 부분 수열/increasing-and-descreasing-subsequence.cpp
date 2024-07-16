c9b5bf0b3f74e0b9bedcaf24bf34c5f0d6506c4b
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-07-16 20:59:24 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240716/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
+++ 240716/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int n;
 int arr[1001];
 int dp_inc[1001]; // dp[i]: i번째가 마지막 원소라고 했을 때 증가하는 부분 수열의 길이
-int dp_dec[1001]; // dp[i]: i번째가 시작 원소라고 했을 때 감소하는 부분 수열의 길이
+int dp_dec[1001]; // dp[i]: i번째가 마지막 원소라고 했을 때 감소하는 부분 수열의 길이
 
 int main() {
     // input
@@ -31,15 +31,16 @@ int main() {
     // 2. dp_dec 계산
     // 5 4 3 2 1
     for(int i=1; i<n; ++i){
-        for(int j=i; j<n; ++j){
-            if(arr[j]<arr[i]) dp_dec[i]=max(dp_dec[i], dp_dec[j]+1);
+        for(int j=0; j<i; ++j){
+            if(arr[j]>arr[i]) dp_dec[i]=max(dp_dec[i], dp_dec[j]+1);
         }
     }
 
     // output
     int answer=0;
     for(int i=0; i<n; ++i){
-        answer = max(answer, dp_inc[i]+dp_dec[i]);
+        int maxNum = max(dp_inc[i]+dp_dec[i]-1, max(dp_inc[i], dp_dec[i]));
+        answer = max(answer, maxNum);
     }
     cout << answer << '\n';
     return 0;
