011ec335e455bfc1468436c68fbb0f23cdeab2f7
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-07-16 21:13:47 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240716/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
+++ 240716/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int n;
 int arr[1001];
 int dp_inc[1001]; // dp[i]: i번째가 마지막 원소라고 했을 때 증가하는 부분 수열의 길이
-int dp_dec[1001]; // dp[i]: i번째가 마지막 원소라고 했을 때 감소하는 부분 수열의 길이
+int dp_dec[1001]; // dp[i]: i번째가 시작 원소라고 했을 때 감소하는 부분 수열의 길이
 
 int main() {
     // input
@@ -22,7 +22,7 @@ int main() {
     // solution
     // 1. dp_inc 계산
     // 1 2 3 4 5
-    for(int i=1; i<n; ++i){
+    for(int i=0; i<n; ++i){
         for(int j=0; j<i; ++j){
             if(arr[j]<arr[i]) dp_inc[i]=max(dp_inc[i], dp_inc[j]+1);
         }
@@ -30,9 +30,13 @@ int main() {
 
     // 2. dp_dec 계산
     // 5 4 3 2 1
-    for(int i=1; i<n; ++i){
-        for(int j=0; j<i; ++j){
-            if(arr[j]>arr[i]) dp_dec[i]=max(dp_dec[i], dp_dec[j]+1);
+    for(int i=0; i<n; ++i){
+        int lastNum=arr[i];
+        for(int j=i+1; j<n; ++j){
+            if(arr[i]>arr[j] && lastNum>arr[j]) {
+                dp_dec[i]=max(dp_dec[i], dp_dec[i]+1);
+                lastNum=arr[j];
+            }
         }
     }
 
@@ -43,5 +47,6 @@ int main() {
         answer = max(answer, maxNum);
     }
     cout << answer << '\n';
+
     return 0;
 }
