890eebad11bfc738a3f8fed2d477f9ea39235a4b
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-07-18 18:49:03 +0900

    Title: 올바른 등식 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240718/올바른 등식 만들기/right-equality.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#define OFFSET 20
+using namespace std;
+
+int n, m;
+
+// 합이 m이 되는 서로다른 가짓수
+// 더하거나 빼는 도중 -20~20 범위를 벗어나면 안 됨
+int main() {
+    // input
+    cin >> n >> m;
+    vector<int> num(n);
+    // dp[i][j]: i번째 인덱스를 고려했을 때 합이 j가 되는 경우의 수
+    vector<vector<long> > dp(n+1, vector<long>(41, 0));
+    for(int i=0; i<n; ++i){
+        cin >> num[i];
+    }
+    
+    // solution
+    dp[0][OFFSET+num[0]] = 1;
+    dp[0][OFFSET-num[0]] = 1;
+    for (int idx = 1; idx < n; ++idx) {
+        for (int sum = -20; sum <= 20; ++sum) {
+            int offsetNum=sum+OFFSET;
+            if(0<=offsetNum-num[idx] && offsetNum-num[idx]<=40){
+                dp[idx][offsetNum]+=dp[idx-1][offsetNum-num[idx]];
+            }
+            if(0<=offsetNum+num[idx] && offsetNum+num[idx]<=40){
+                dp[idx][offsetNum]+=dp[idx-1][offsetNum+num[idx]];
+            }
+        }
+    }
+
+    // output
+    cout << dp[n-1][m+OFFSET] <<'\n';
+    return 0;
+}
