c7bd369aa1f13c99a727761c0647e29fa29e0d17
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-07-30 14:16:58 +0900

    Title: 낮은 지점들, Time: 55ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240730/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map> 
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    // 가장 작은 y값을 저장
+    unordered_map<int, int> um;
+    for(int i=0; i<n; ++i){
+        int x, y;
+        cin >> x >> y;
+        if(um.find(x)==um.end() || um[x]>y) um[x]=y;
+    }
+
+    // 남아있는 y값 합 출력
+    int answer=0;
+    for(auto it=um.begin(); it!=um.end(); ++it){
+        answer+=it->second;
+    }
+    cout << answer <<'\n';
+    return 0;
+}
