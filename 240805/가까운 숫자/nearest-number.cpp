fdddbaac207df49a2281e2d6120493047a98048f
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-08-05 21:17:32 +0900

    Title: 가까운 숫자, Time: 220ms, Memory: 4MB, Status: Passed - CodeTree

--- 240805/가까운 숫자/nearest-number.cpp
+++ 240805/가까운 숫자/nearest-number.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <set>
+#include <climits>
 
 using namespace std;
 
@@ -9,19 +9,25 @@ int main() {
     cin >> n;
 
     set<int> s;
-    vector<int> vec(n);
-    vec.push_back(0); // 처음 수직선 상에 x = 0 위치에만 점이 하나 
+    s.insert(0);
 
+    int minDist=INT_MAX;
     // n번에 걸쳐 해당 숫자의 위치에 점을 추가한 이후 가장 가까운 두 점 사이의 거리를 한 줄에 하나씩 출력
     for(int i=0; i<n; ++i){
         int num;
         cin >> num;
-        for(auto e:vec){
-            int dist = abs(e-num);
-            s.insert(dist);
-        }
-        vec.push_back(num);
-        cout << *s.begin() << '\n';
+        s.insert(num);
+
+        // num의 위치와 num이전의 위치, num 다음 위치를 구한다.
+        auto it = s.find(num);
+        auto prev_it = (it==s.begin()) ? s.end() : prev(it);
+        auto next_it = (it==s.end()) ? s.end() : next(it);
+        
+        if(prev_it!=s.end()) minDist = min(minDist, num-*prev_it);
+        if(next_it!=s.end()) minDist = min(minDist, *next_it-num);
+
+
+        cout << minDist << '\n';
     }
 
     return 0;
