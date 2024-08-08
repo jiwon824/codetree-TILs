cdb71316bbd69b394d24d230f1fac8b535752add
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-08-08 19:22:24 +0900

    Title: 마지막으로 남은 숫자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240808/마지막으로 남은 숫자/last-remaining-number.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    priority_queue<int> pq;
+    // n개의 숫자
+    for(int i=0; i<n; ++i){
+        int num;
+        cin >> num;
+        pq.push(num);
+    }
+
+    // 2개 이상의 숫자가 남아 있는 한 계속 반복
+    while(pq.size()>=2){
+        // 가장 큰 숫자 2개를 뽑아 제거하고 두 숫자의 차이에 해당하는 숫자를 다시 집어넣는 것
+        int num1=pq.top();
+        pq.pop();
+        int num2=pq.top();
+        pq.pop();
+        if(num1!=num2) pq.push(num1-num2);
+    }
+
+    cout << pq.top() << '\n';
+    return 0;
+}
