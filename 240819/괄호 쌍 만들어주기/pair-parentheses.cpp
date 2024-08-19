43b20ae72a9a99169e742d83f522ac5bbb98ae23
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-08-19 13:52:22 +0900

    Title: 괄호 쌍 만들어주기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240819/괄호 쌍 만들어주기/pair-parentheses.cpp
+++ 240819/괄호 쌍 만들어주기/pair-parentheses.cpp
@@ -3,26 +3,17 @@
 using namespace std;
 
 int opening[MAX_N];// 연속((
-int closing[MAX_N]; // 연속))
 
 int main() {
     string a;
     cin >> a;
-    // 연속 여는 괄호 체크
+    
+    int answer=0;
     for(int i=1; i<a.length(); ++i){
         if(a[i-1]=='(' && a[i]=='(') opening[i]=opening[i-1]+1;
         else opening[i]=opening[i-1];
+        if (a[i-1]==')' && a[i]==')') answer += opening[i];
     }
-    // 연속 닫는 괄호 체크
-    for(int i=a.length()-2; i>=0; --i){
-        if(a[i+1]==')' && a[i]==')') closing[i]=closing[i+1]+1;
-        else closing[i]=closing[i+1];
-    }
-
-    int answer=0;
-    for(int i=1; i<a.length(); ++i){
-        answer = max(answer, opening[i]*closing[i+1]);
-    }
-    cout << answer <<'\n';
+    cout << answer << '\n';
     return 0;
 }
