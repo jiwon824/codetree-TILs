ff527c9e224ee0ebc272074310579c7cf9f9c576
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-08-23 18:30:31 +0900

    Title: 정수 두 개의 합 2, Time: 44ms, Memory: 0MB, Status: Passed - CodeTree

--- 240823/정수 두 개의 합 2/sum-of-two-integers-2.cpp
+++ 240823/정수 두 개의 합 2/sum-of-two-integers-2.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -7,35 +6,26 @@ using namespace std;
 const int MAX_N = 100001;
 
 int n, k;
-vector<int> num;
+int num[MAX_N];
 
 int main() {
     // input
     cin >> n >> k;
-    num.resize(n);
     for(int i=0; i<n; ++i){
         cin >> num[i];
     }
     
-    // 정렬
-    sort(num.begin(), num.end());
+    sort(num, num+n);
 
-    // 가능한 최대 r idx 구하기 num[r]<=k인 최초 idx
-    int lastRIdx=n, r=n;
-    for(int i=n-1; i>0; --i){
-        if(num[i]<=k){
-            lastRIdx=i;
-            break;
-        }
-    }
+    int r=n-1, answer=0;
+    for(int l=0; l<r; ){
+        int sum = num[l]+num[r];
 
-    int answer=0;
-    for(int l=0; num[l]<k; ++l){
-        for(int r=lastRIdx; r>l; --r){
-            if(num[l]+num[r]<=k){
-                answer++;
-            }
+        if(sum<=k) {
+            answer+=(r-l);
+            l++;
         }
+        else r--;
     }
     cout << answer << '\n';
     return 0;
