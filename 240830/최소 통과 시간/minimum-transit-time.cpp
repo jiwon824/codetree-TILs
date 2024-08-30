484122fad321b447cacc26aec2e2a42aa7f94e01
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-08-30 22:44:00 +0900

    Title: 최소 통과 시간, Time: 28ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240830/최소 통과 시간/minimum-transit-time.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int MAX_M = 100000;
+
+int n, m;
+int t[MAX_M];
+
+bool IsPossible(long long time){
+    int cnt =0;// 물건의 개수
+    for(int i=0; i<m; ++i){
+        cnt+=time/t[i];
+        if(cnt>=n) return true;
+    }
+    return false;
+}
+
+int main() {
+    cin >> n >> m;
+    for(int i=0; i<m; ++i){
+        cin >> t[i];
+    }
+
+    sort(t,t+m);
+    
+    long long left=1, right=t[m-1]*n, answer=t[m-1]*n;
+    while(left<=right){
+        long long mid = (left+right)/2;
+        if(IsPossible(mid)){
+            right=mid-1;
+            answer = mid;
+        }
+        else left=mid+1;
+    }
+
+    cout << answer << '\n';
+    return 0;
+}
