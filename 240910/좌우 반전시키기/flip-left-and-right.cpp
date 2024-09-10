e5aedaaef431d465b3f0f572cc48af516508bc6f
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-09-10 21:07:13 +0900

    Title: 좌우 반전시키기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240910/좌우 반전시키기/flip-left-and-right.cpp
+++ 240910/좌우 반전시키기/flip-left-and-right.cpp
@@ -3,11 +3,10 @@ using namespace std;
 
 const int MAX_N = 101;
 int n;
-char num[MAX_N];
+int num[MAX_N];
 
 void Change(int idx){
-    if(num[idx]=='0') num[idx]='1';
-    else num[idx]='0';
+    num[idx]^=1;
 }
 
 int main() {
@@ -18,7 +17,7 @@ int main() {
     
     int checkIdx=1, answer=0;
     while(checkIdx<n){
-        if(num[checkIdx-1]=='0'){
+        if(num[checkIdx-1]==0){
             for(int i=-1; i<=1; ++i){
                 Change(checkIdx+i);
             }
@@ -27,7 +26,7 @@ int main() {
         checkIdx++;
     }
 
-    if(num[n-1]=='0') answer=-1;
+    if(num[n-1]==0) answer=-1;
     cout << answer << '\n';
     return 0;
 }
