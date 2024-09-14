ff618fd544d3d6c8297f323141fe55b5cc2de198
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-09-14 22:41:12 +0900

    Title: 가장 오래 걸리는 학생, Time: 177ms, Memory: 6MB, Status: Passed - CodeTree

---
+++ 240914/가장 오래 걸리는 학생/longest-student.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+const int MAX_N = 100001;
+
+int n, m;
+
+vector<vector<pair<int, int>>> graph(MAX_N); // graph[i][j]={vertex, dist} i->vertex까지 dist
+priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;// {mindist, vertex}
+int dist[MAX_N];
+
+void InitDist(){ 
+    // n-1에서 다른 모든 정점으로의 거리를 구해야 함 -> n-2까지 초기화
+    for(int i=0; i<n-1; ++i){
+        dist[i]=1e9;
+    }
+}
+
+int main() {
+    // input
+    cin >> n >> m;
+    for(int i=0; i<m; ++i){
+        int a, b, d;
+        cin >> a >> b >> d;
+
+        graph[a-1].push_back({b-1, d});
+        graph[b-1].push_back({a-1, d});
+    }
+
+    // solution
+    InitDist();
+    // 출발지점과 거리를 queue에 push
+    pq.push({0, n-1});
+
+    while(!pq.empty()){
+        int min_dist = pq.top().first, min_vertex=pq.top().second;
+        pq.pop();
+
+        // 이미 갱신된 상태라면 패스
+        // (같은 정점의 원소가 여러 번 들어가는 문제가 있을 수 있어서 처리)
+        if(min_dist != dist[min_vertex]) continue;
+
+        for(int j=0; j<graph[min_vertex].size(); ++j){
+            // graph[i][j]={vertex, dist} i->vertex까지 dist
+            int v=graph[min_vertex][j].first, d=graph[min_vertex][j].second;
+            int new_dist = dist[min_vertex]+d;
+            dist[v]=min(dist[v], new_dist);
+            // {mindist, vertex}
+            pq.push({new_dist, v});
+        }
+    }
+
+    // output
+    int answer=0;
+    for(int i=0; i<n; ++i){
+        if(dist[i]==1e9) dist[i]=-1;
+        answer=max(answer, dist[i]);
+    }
+    cout << answer << '\n';
+    return 0;
+}
