1c3cf8db3fd0cd99a694df9e6b73f2e848036268
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-10-02 20:29:57 +0900

    Title: 트리 중위 순회, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241002/트리 중위 순회/tree-inorder.cpp
+++ 241002/트리 중위 순회/tree-inorder.cpp
@@ -52,3 +52,54 @@ int main() {
 
     return 0;
 }
+
+/* [해설코드]
+
+#include <iostream>
+
+#define MAX_N 1024
+
+using namespace std;
+
+// 변수 선언
+int k, n;
+int a[MAX_N + 1];
+int tree_num[MAX_N + 1];
+int cnt = 1;
+
+// 중위 순회를 진행하여 각 트리의 위치에 맞는 번호를 채워 넣습니다.
+void InOrder(int x) {
+    // n보다 큰 노드는 없습니다. 빠져나갑니다.
+    if(x > n)
+        return;
+
+    InOrder(x * 2);
+    tree_num[x] = a[cnt++];
+    InOrder(x * 2 + 1);
+}
+
+int main() {
+    // 입력:
+    cin >> k;
+
+    // 2^i은 (1 << i)로 쉽계 계산이 가능합니다.
+    n = (1 << k) - 1;
+
+    // a라는 배열에 중위 순회 순서를 입력받습니다.
+    for(int i = 1; i <= n; i++)
+        cin >> a[i];
+
+    // 중위 순회를 진행하여 각 트리의 위치에 맞는 번호를 채워 넣습니다.
+    InOrder(1);
+
+    // 트리의 구조에 맞게 출력을 진행합니다.
+    for(int i = 1; i <= k; i++) {
+        for(int j = (1 << (i - 1)); j <= (1 << i) - 1; j++) {
+            cout << tree_num[j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+*/
