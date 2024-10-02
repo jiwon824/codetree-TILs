fa2c8f8ce61aab56d63b1047d80653984b6a05da
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-10-02 13:55:37 +0900

    Title: 트리 순회, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241002/트리 순회/the-tree-traversal.cpp
+++ 241002/트리 순회/the-tree-traversal.cpp
@@ -1,76 +1,75 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-
 using namespace std;
 
 const int MAX_N = 27;
 
 int n;
-char arr[MAX_N][3]; // {node, left, right}
-vector<char> tree;
+char arr[MAX_N][3];
 
-// Creating a tree using arr
-void MakeTree(){
-    // 추가된 값은 '\0'값으로 초기화된다.
-    tree.resize(pow(2, n+1));
-    tree[1]=arr[0][0], tree[2]=arr[0][1], tree[3]=arr[0][2];
+// node, left, right
+void Preorder(int idx){
+    char node=arr[idx][0], left=arr[idx][1], right=arr[idx][2];
 
-    for(int i=1; i<n; ++i){
-        char node=arr[i][0], left=arr[i][1], right=arr[i][2];
-        for(int j=0; j<(int)tree.size(); ++j){
-            // 노드를 찾으면 left, right를 정해진 인덱스에 넣고 
-            if(tree[j]==node){
-                tree[j*2]=left;
-                tree[j*2+1]=right;
-                break;
-            }
-        }
+    // node
+    cout << node;
+    // left
+    for(int i=0; i<n; ++i){
+        if(left=='.') break;
+        if(arr[i][0]==left) Preorder(i);
+    }
+    // right
+    for(int i=0; i<n; ++i){
+        if(right=='.') break;
+        if(arr[i][0]==right) Preorder(i);
+    }
+}
+// left, node, right
+void Inorder(int idx){
+    char node=arr[idx][0], left=arr[idx][1], right=arr[idx][2];
+
+    // left
+    for(int i=0; i<n; ++i){
+        if(left=='.') break;
+        if(arr[i][0]==left) Inorder(i);
+    }
+    // node
+    cout << node;
+    // right
+    for(int i=0; i<n; ++i){
+        if(right=='.') break;
+        if(arr[i][0]==right) Inorder(i);
     }
 }
 
-// node left right
-void Preorder(int idx){
-    if(tree[idx]=='\0' || tree[idx]=='.') return;
-
-    cout << tree[idx];
-    Preorder(idx*2);
-    Preorder(idx*2+1);
-}
-
-// left node right
-void Inorder(int idx){
-    if(tree[idx]=='\0' || tree[idx]=='.') return;
-
-    Inorder(idx*2);
-    cout << tree[idx];
-    Inorder(idx*2+1);
-}
-
-// left right node
+// left, right, node
 void Postorder(int idx){
-    if(tree[idx]=='\0' || tree[idx]=='.') return;
-    
-    Postorder(idx*2);
-    Postorder(idx*2+1);
-    cout << tree[idx];
+    char node=arr[idx][0], left=arr[idx][1], right=arr[idx][2];
+
+    // left
+    for(int i=0; i<n; ++i){
+        if(left=='.') break;
+        if(arr[i][0]==left) Postorder(i);
+    }
+    // right
+    for(int i=0; i<n; ++i){
+        if(right=='.') break;
+        if(arr[i][0]==right) Postorder(i);
+    }
+    // node
+    cout << node;
 }
 
 int main() {
-    // input
     cin >> n;
     for(int i=0; i<n; ++i){
         cin >> arr[i][0] >> arr[i][1] >> arr[i][2];
     }
 
-    MakeTree();
-
-    // output
-    Preorder(1);
+    Preorder(0);
     cout << '\n';
-    Inorder(1);
+    Inorder(0);
     cout << '\n';
-    Postorder(1);
+    Postorder(0);
     cout << '\n';
     return 0;
 }
