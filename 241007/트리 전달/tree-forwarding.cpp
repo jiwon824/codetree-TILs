daebf52348776fd92fa0b2aada286f78d56030d6
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-10-07 22:09:10 +0900

    Title: 트리 전달, Time: 81ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241007/트리 전달/tree-forwarding.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int MAX_N = 100001;
+
+int n, m;
+int parent[MAX_N];
+int score[MAX_N];// i번 노드의 초기 점수
+int dp[MAX_N]; // dp[i]=dp[parent[i]]+score[i] ==부모의 점수+내 점수
+
+int main() {
+    cin >> n >> m;
+    for(int i=1; i<=n; ++i){
+        cin >> parent[i];
+    }
+    // 연산의 수
+    for(int i=0; i<m; ++i){
+        int node, num;
+        cin >> node >> num;
+        score[node]+=num;
+    }
+
+    // solution
+    for(int i=1; i<=n; ++i){
+        if(parent[i]==-1) continue;
+        dp[i]=dp[parent[i]]+score[i];
+    }
+
+    // output
+    for(int i=1; i<=n; ++i){
+        cout << dp[i] << " ";
+    }
+    return 0;
+}
