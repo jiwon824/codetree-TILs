2e6321c4dc7c8589a55801c364432c22dc045987
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-10-13 15:04:20 +0900

    Title: 인접한 정점, Time: 22ms, Memory: 1MB, Status: Passed - CodeTree

--- 241013/인접한 정점/adjacent-node.cpp
+++ 241013/인접한 정점/adjacent-node.cpp
@@ -24,44 +24,15 @@ void DFS(int x){
     }
 
     dp[x][0]=0, dp[x][1]=weight[x];
-    bool child_selected = false;
 
     for(int i=0; i<(int)edges[x].size(); ++i){
         int y = edges[x][i];
         if(parent[y]==x){
-            // x를 선택하는 경우=자식을 선택하지 않은 경우
             dp[x][1]+=dp[y][0];
-
-            // 자식을 선택할 것인지 말 것인지 고려
-            // 자식을 선택한 게 자식을 선택하지 않은 것보다 크다면 부모선택x-자식 선택o
-            if(dp[y][1] > dp[y][0]) {
-                dp[x][0] += dp[y][1];
-                child_selected = true;
-            }
-            else dp[x][0]+= dp[y][0];
+            dp[x][0]+= max(dp[y][1], dp[y][0]);
         }
     }
-
-    // 자식을 하나도 선택하지 않았다면(=모든 자식을 dp[x][0]+= dp[y][0]한 경우),
-    // 가장 이득이 큰 자식 하나를 강제로 선택
-
-    // dp[x][0]에 모든 자식을 선택하지 않는 경우(dp[y][0])를 더했기 때문에
-    // 이득이 큰 자식을 선택하려면, 선택하는 자식에 대해 아래 연산을 수행해야 한다.
-    // dp[x][0]=dp[x][0]-dp[y][0]+dp[y][1]
-
-    // -dp[y][0]+dp[y][1]=max_diff가 되고, dp[x][0]과 합쳐주는 연산을 취하므로
-    // dp[x][0]+=max_diff
-    if(!child_selected){
-        int max_diff = 0;
-        for(int i=0; i<(int)edges[x].size(); ++i) {
-            int y = edges[x][i];
-            if(parent[y] == x) {
-                max_diff = max(max_diff, dp[y][1] - dp[y][0]);
-            }
-        }
-        dp[x][0]+= max_diff;
-    }
-
+    
 }
 int main() {
     // input
