24358eaf4c793aa0fa9b351ededdff297bca439d
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-10-13 22:41:27 +0900

    Title: 이진 트리 최대 경로 합, Time: 41ms, Memory: 2MB, Status: Passed - CodeTree

--- 241013/이진 트리 최대 경로 합/binary-tree-maximum-path-sum.cpp
+++ 241013/이진 트리 최대 경로 합/binary-tree-maximum-path-sum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,34 +11,36 @@ int n;
 int weight[MAX_N]; // weight[i]: i의 weight
 vector<int> edges[MAX_N];
 
-// 문제를 풀기 위해 필요한 것
-int parent[MAX_N]; // parent[i]: i의 부모노드
-bool visited[MAX_N]={false, };
-// 30,000 * 1,000 = 30,000,000 이므로 int로 가능. INT_MAX=2,147,483,647
 // 노드 i를 지나는 가장 긴 경로합은 노드 i의 왼쪽 자식 L, 오른쪽 자식 R일 때
 // "L을 루트로 하는 서브트리에서 L를 지나는 가장 긴 경로의 경로합
 // + R을 루트로 하는 서브트리에서 R을 지나는 가장 긴 경로합"
 int max_length[MAX_N];
+int answer = -30000000;
 
 void DFS(int x){
-    for(int i=0; i<(int)edges[x].size(); ++i){
-        int y =edges[x][i];
-        if(visited[y])continue;
-        visited[y]=true;
-        parent[y]=x;
-        DFS(y);
+    // 자식이 없는 경우
+    if(edges[x].empty()) {
+        max_length[x]=weight[x];
+        answer=max(answer, max_length[x]);
+        return;
     }
 
-    // x를 지나는 가장 긴 경로의 합
-    // x를 지나기 때문에 일단 x의 가중치로 초기화
-    max_length[x]=weight[x];
-    for(int i=0; i<(int)edges[x].size(); ++i){
-        int y =edges[x][i];
-        if(parent[y]==x && max_length[y]>0){
-            max_length[x]+=max_length[y];
-        }
+    int num_of_child = (int)edges[x].size();
+    int left_sum = 0, right_sum = 0;
+    if(num_of_child >= 1){
+        int left_child = edges[x][0];
+        DFS(left_child);
+        left_sum = max(0, max_length[left_child]);
+    }
+    if(num_of_child==2){
+        int right_child = edges[x][1];
+        DFS(right_child);
+        right_sum = max(0, max_length[right_child]);
     }
 
+    max_length[x]=max(max_length[x], weight[x]+max({0, left_sum, right_sum}));
+
+    answer = max(answer, weight[x] + left_sum + right_sum);
 }
 
 int main() {
@@ -50,7 +53,6 @@ int main() {
         cin >> a >> b;
         // 간선 연결
         edges[a].push_back(b);
-        edges[b].push_back(a);
     }
     // 총 N개의 줄에 걸쳐 각 노드들이 가지는 값이 주어집니다.
     for(int i=1; i<=n; ++i){
@@ -59,14 +61,9 @@ int main() {
 
     // solution
     // 트리의 루트는 1번
-    visited[1]=true;
     DFS(1);
 
     // output
-    int answer = -30000000;
-    for(int i=1; i<=n; ++i){
-        answer=max(answer, max_length[i]);
-    }
     cout << answer << '\n';
     return 0;
 }
