aef818e02634fd27ec36b2e6c915eadfc39515ce
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-10-14 21:03:45 +0900

    Title: 인접한 노드 2, Time: 19ms, Memory: 1MB, Status: Passed - CodeTree

--- 241014/인접한 노드 2/adjacent-node-2.cpp
+++ 241014/인접한 노드 2/adjacent-node-2.cpp
@@ -14,7 +14,7 @@ int num[MAX_N]; // num[i]: i번 노드에 적힌 값
 int dp[MAX_N][2]; // dp[i][j]: j=0 선택x 경우, j=1 선택o 경우
 vector<int> picked;
 
-void DFS(int x){
+void FillDpTable(int x){
     dp[x][1]=num[x];
 
     for(int i=0; i<(int)edges[x].size(); ++i){
@@ -22,7 +22,7 @@ void DFS(int x){
         if(visited[y])continue;
 
         visited[y]=true;
-        DFS(y);
+        FillDpTable(y);
 
         // x번 노드가 포함된 경우 y번 노드는 포함될 수 없습니다.
         // x번 노드가 포함되지 않은 경우 y번 노드는 포함될 수도,
@@ -30,10 +30,21 @@ void DFS(int x){
         dp[x][1] += dp[y][0];
         dp[x][0] += max(dp[y][0], dp[y][1]);
     }
+}
 
-    // x를 선택하는 게 나은 선택이라면 x 저장
-    if(dp[x][1]>dp[x][0]) {
+void PickNode(int x, int parent, bool pickFlag){
+    // 노드 선택 여부 결정
+    if (!pickFlag && dp[x][1] > dp[x][0]) {
         picked.push_back(x);
+        pickFlag = true;
+    }
+    else pickFlag = false; 
+
+    // 자식 노드 탐색
+    for(int i=0; i<(int)edges[x].size(); ++i){
+        int y=edges[x][i];
+        if(y==parent) continue;
+        PickNode(y, x, pickFlag);
     }
 }
 
@@ -53,7 +64,10 @@ int main() {
 
     // solution
     visited[1]=true;
-    DFS(1);
+    FillDpTable(1);
+
+    PickNode(1, 0, false);
+
     sort(picked.begin(), picked.end());
 
     // output
