22a199ff9d1085e081efea311ff9770cb457ce67
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-10-18 14:24:41 +0900

    Title: 트리 경로 길이 2, Time: 525ms, Memory: 19MB, Status: Passed - CodeTree

--- 241018/트리 경로 길이 2/tree-path-length-2.cpp
+++ 241018/트리 경로 길이 2/tree-path-length-2.cpp
@@ -12,6 +12,7 @@ bool visited[MAX_N];
 
 int depth[MAX_N];
 int parent[MAX_H][MAX_N];
+long long dist[MAX_N]; // 루트에서 i번 노드까지의 가중치의 합
 
 void dfs(int x){
     for(pair<int,int> child : edges[x]){
@@ -20,6 +21,7 @@ void dfs(int x){
 
         visited[y]=true;
         depth[y]=depth[x]+1;
+        dist[y] += dist[x]+child.second;
         parent[0][y]=x;
         dfs(y);
     }
@@ -45,24 +47,8 @@ int getLca(int a, int b){
     return parent[0][a];
 }
 
-int sumWeightsBetweenNodes(int target, int x){
-    int answer=0;
-    while(target!=x){
-        int px=parent[0][x];
-        // x랑 px가 연결된 간선을 찾아서 가중치를 answer에 더해줌
-        for(pair<int,int> e : edges[x]){
-            if(e.first==px){
-                answer+=e.second;
-                break;
-            }
-        }
-        // x 한 칸 올리기
-        x=px;
-    }
-    return answer;
-}
-
 int main() {
+    // input
     cin >> n;
     for(int i=1; i<n; ++i){
         int a, b, w;
@@ -87,9 +73,7 @@ int main() {
         int a, b;
         cin >> a >> b;
         int lca = getLca(a, b);
-
-        int answer=sumWeightsBetweenNodes(lca, a)+sumWeightsBetweenNodes(lca, b);
-        cout << answer << '\n';
+        cout << dist[a]+dist[b]-2*dist[lca] << '\n';
     }
     return 0;
 }
