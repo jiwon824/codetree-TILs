fdc12332d5bc1bb327f50f71d6b4f907aea40643
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-10-23 14:46:17 +0900

    Title: 특정한 정점과 연결하지 않기, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 241023/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.cpp
+++ 241023/특정한 정점과 연결하지 않기/do-not-connect-with-a-specific-node.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 const int MAX_N =100'000;
@@ -37,12 +39,31 @@ int main() {
     }
     cin >> nodeA >> nodeB >> k;
 
-    // 추가로 최대 k개의 간선을 놓아 정점 A가 최대한 많은 정점과 연결되게
+    // B와 겹쳐지지 않는 노드들의 size를 components에 push
     // 하지만 간선을 놓을 때 정점 B와는 연결되지 않도록
+    vector<int> components;
+    int visited[MAX_N]={false, };
     for(int i=1; i<=n; ++i){
-        if(myFind(i)==myFind(nodeB)) continue;
-        myUnion(nodeA, i);
+        int root_i = myFind(i);
+
+        // b랑 연결된 노드는 a 노드랑 연결하면 안 됨.
+        if(root_i==myFind(nodeB)) continue;
+        // 이미 방문한 노드거나 a노드에 연결된 노드라면 추가하지 않음
+        if(visited[root_i] || root_i==myFind(nodeA)) continue;
+
+        visited[root_i]=true;
+        components.push_back(size[root_i]);
     }
-    cout << size[myFind(nodeA)] << '\n';
+
+    // size[root]가 큰 순서대로 정렬
+    sort(components.begin(), components.end(), greater<>());
+
+    int answer=size[myFind(nodeA)];
+    for(int i=0; i<k; ++i){
+        answer+=components[i];
+    }
+    // 놓을 수 있는 간선 중 크기가 큰 것을 
+    // 최대 k개의 간선을 놓아 정점 A가 최대한 많은 정점과 연결되게
+    cout << answer << '\n';
     return 0;
 }
