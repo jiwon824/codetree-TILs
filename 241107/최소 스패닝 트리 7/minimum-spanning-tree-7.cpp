383f217814d2cd6cdbf2da44cdc5b5bb77d7b287
Jiwon <59115860+jiwon824@users.noreply.github.com>
2024-11-07 17:18:18 +0900

    Title: 최소 스패닝 트리 7, Time: 60ms, Memory: 4MB, Status: Passed - CodeTree

--- 241107/최소 스패닝 트리 7/minimum-spanning-tree-7.cpp
+++ 241107/최소 스패닝 트리 7/minimum-spanning-tree-7.cpp
@@ -5,8 +5,10 @@
 using namespace std;
 
 const int MAX_N = 40'000;
+
 int n, m, mst_sum, max_route, far_node;
 vector<pair<int,int> > edges[MAX_N+1]; // edges[a]={b, w}
+vector<pair<int,int> > mst_edges[MAX_N+1];
 int dist[MAX_N+1];
 bool visited[MAX_N+1]={false, };
 
@@ -21,44 +23,52 @@ void initDist(){
 void dfs(int curr_node, int dist){
     if(visited[curr_node]) return;
     visited[curr_node]=true;
-
+    
     if(dist>max_route){
         max_route=dist;
         far_node=curr_node;
     }
-
-    // 현재 노드에 연결된 노드 탐색
-    for(auto e : edges[curr_node]){
+    
+    // mst_edges를 사용하도록 수정
+    for(auto e : mst_edges[curr_node]){
         int v=e.first, w=e.second;
         dfs(v, dist+w);
     }
 }
 
 void prim(){
-    priority_queue<pair<int,int>, vector<pair<int,int> >, greater<>> pq;//{weight, node}
+    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq; //{weight, node}
+    vector<int> parent(n+1, -1);  // 각 노드의 부모 노드를 저장
     
     // 1번 노드에서 시작
     dist[1]=0;
     pq.push({0, 1});
-
+    
     while(!pq.empty()){
         int min_weight = pq.top().first, min_vertex = pq.top().second;
         pq.pop();
-
+        
         if(visited[min_vertex]) continue;
         visited[min_vertex]=true;
         mst_sum+=min_weight;
-
+        
+        // parent가 있다면 (시작노드가 아니라면) mst_edges에 추가
+        int parent_of_min_vertex = parent[min_vertex];
+        if(parent_of_min_vertex != -1) {
+            mst_edges[parent_of_min_vertex].push_back({min_vertex, min_weight});
+            mst_edges[min_vertex].push_back({parent_of_min_vertex, min_weight});
+        }
+        
         // min_vertex에 이어진 간선 탐색
         for(auto e: edges[min_vertex]){
             int v=e.first, w=e.second;
             if(!visited[v] && dist[v]>w){
                 dist[v]=w;
+                parent[v] = min_vertex;  // v의 부모를 min_vertex로 설정
                 pq.push({w, v});
             }
         }
     }
-
 }
 
 int main() {
@@ -79,8 +89,10 @@ int main() {
 
     // [solution] 트리의 지름 구하기
     fill(visited, visited+(n+1), false);
+    max_route=0;
     dfs(1, 0);
     fill(visited, visited+(n+1), false);
+    max_route=0;
     dfs(far_node, 0);
 
     // [output]
